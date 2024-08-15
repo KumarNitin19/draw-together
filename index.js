de4a43f0df91984e3b80952157edce9d6548dd37
KumarNitin19 <nitinkumardeshmukh19@gmail.com>
2024-08-15 22:52:06 +0530

    feat: basic function added for start and stop drawing

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 console.log("Drawing app");
 
+let isDrawing = false;
+
 const canvas = document.querySelector("canvas"),
   ctx = canvas.getContext("2d");
 
-window.addEventListener("load", function (e) {
-  canvas.width = e.offsetWidth;
-  canvas.height = e.offsetHeght;
+window.addEventListener("load", function () {
+  canvas.width = canvas.offsetWidth;
+  canvas.height = canvas.offsetHeight;
 });
 
+function startDrawing() {
+  isDrawing = true;
+}
+
+function stopDrawing() {
+  isDrawing = false;
+}
+
 const drawing = (e) => {
+  if (!isDrawing) return; // if drawing false then return from here!
   ctx.lineTo(e.offsetX, e.offsetY); // creating line according to mouse pointer
   ctx.stroke(); // draw/filling line with color
 };
 
+canvas.addEventListener("mousedown", startDrawing);
 canvas.addEventListener("mousemove", drawing);
+canvas.addEventListener("mouseup", stopDrawing);
