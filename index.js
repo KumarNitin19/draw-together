d9452ce84b104387371d310e9f7b94d47f51de1d
KumarNitin19 <nitinkumardeshmukh19@gmail.com>
2024-08-15 22:45:10 +0530

    feat: basic canvas and its action added

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 console.log("Drawing app");
+
+const canvas = document.querySelector("canvas"),
+  ctx = canvas.getContext("2d");
+
+window.addEventListener("load", function (e) {
+  canvas.width = e.offsetWidth;
+  canvas.height = e.offsetHeght;
+});
+
+const drawing = (e) => {
+  ctx.lineTo(e.offsetX, e.offsetY); // creating line according to mouse pointer
+  ctx.stroke(); // draw/filling line with color
+};
+
+canvas.addEventListener("mousemove", drawing);
